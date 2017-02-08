de77122c4114a5ba457270d11c11f1d81d5c6cdf
Sasank Sekhar Panda <sasanksekhar.ab@gmail.com>
2017-02-08 15:15:50 +0530

    Create Introduction_demo.cpp

---
+++ Introduction_demo.cpp
@@ -0,0 +1,28 @@
+#include <GL/freeglut.h>
+#include <Windows.h>
+void display()
+{
+		glClearColor(1.0f,0.654f,0.260f,1.0f);
+		//Set background alpha and color to black
+		glClear(GL_COLOR_BUFFER_BIT);
+		//clear the color buffer
+		glBegin(GL_POLYGON);
+		glColor3f(0.0f,0.0f,1.0f);
+		glVertex2f(0.0f,0.0f);
+		glVertex2f(0.0f,0.5f);
+		glVertex2f(0.5f,0.5f);
+		glVertex2f(0.5f,0.0f);
+		glEnd();
+		glFlush();//Render the thing
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc,argv);//Initialize glut
+	glutCreateWindow("Open GL Setup Test");
+	glutInitWindowSize(500,500);
+	glutInitWindowPosition(60,50);
+	glutDisplayFunc(display);//Register display call bach handler for window
+	glutMainLoop();
+	return 0;
+}
